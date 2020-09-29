8382af69f0a5ed9fc9417d27ea09b4f282650045
choirul amin <70601788+choiruls@users.noreply.github.com>
2020-09-29 07:47:14 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+//xxx Wajib Diisi
+
+$token = "ZDgyZjYwZGQtYzc1NC00OTM3LWJhYzktMDE4ZTAxNjk3ZTAw";
+$uuid = "287df28c-4042-4d3c-a17b-253b3cce3107";
+$userid = "27098744";
+
+$eksternal_version = "2.0.1";
+$version = "42";
+$device = "8.1.0";
+$timezone = "7";
+$lang = "id";
+
+$spin_version = "40";
+$user_agent = "Mozilla/5.0 (Linux; Android 8.1.0; Redmi 6 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.127 Mobile Safari/537.36"; //Search in Google Chrome With Keyword "my user agent"
